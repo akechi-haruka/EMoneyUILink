2c7bfc21631f2e517123f157089fae409969ac6a
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2024-08-17 12:34:30 +0200

    Add APMHeadbananaLink

---
+++ APMHeadbananaLink/Bananaphone.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace APMHeadbananaLink {
+
+    [BepInPlugin("eu.haruka.apm.bananaphone", "HeadbananaLink", "1.0")]
+    public class Bananaphone : BaseUnityPlugin {
+
+        [DllImport("apmHeadphoneVolume", EntryPoint = "apmHeadbananaVersionGet")]
+        private static extern int ApmHeadbananaVersionGet();
+
+        [DllImport("apmHeadphoneVolume", EntryPoint = "apmHeadphoneVolumeGet")]
+        private static extern float ApmHeadphoneVolumeGet();
+
+        [DllImport("apmHeadphoneVolume", EntryPoint = "apmHeadphoneVolumeSet")]
+        private static extern void ApmHeadphoneVolumeSet(float volume);
+
+        [DllImport("apmHeadphoneVolume", EntryPoint = "apmHeadphoneChannelsSet")]
+        private static extern void ApmHeadphoneChannelsSet([MarshalAs(UnmanagedType.LPArray)]  int[] chanels, int len);
+
+        private ConfigEntry<String> ConfigChannelList;
+
+        public void Awake() {
+
+            ConfigChannelList = Config.Bind("General", "Headphone Channels", "2,3", "A comma seperated list of channels that should be manipulated by APMHeadbanana");
+            ConfigChannelList.SettingChanged += ConfigChannelList_SettingChanged;
+
+            try {
+                Logger.LogInfo("BANANA: version " +  ApmHeadbananaVersionGet());
+            } catch {
+                Logger.LogError("NO BANANA.");
+                return;
+            }
+
+            UpdateChannels();
+        }
+
+        private void ConfigChannelList_SettingChanged(object sender, EventArgs e) {
+            UpdateChannels();
+        }
+
+        private void UpdateChannels() {
+            List<int> channels = new List<int>();
+            foreach (String s in ConfigChannelList.Value.Split(',')) {
+                if (Int32.TryParse(s, out int channel)) {
+                    channels.Add(channel);
+                } else {
+                    Logger.LogWarning("Could not parse channel: " + s);
+                }
+            }
+            if (channels.Count > 0) {
+                ApmHeadphoneChannelsSet(channels.ToArray(), channels.Count);
+                Logger.LogDebug("Channel list updated");
+            } else {
+                Logger.LogError("Channel list is empty");
+            }
+        }
+    }
+}
---
+++ APMHeadbananaLink/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("APMHeadbananaLink")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APMHeadbananaLink")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c91294fd-329f-42d3-a4c2-fddd3721ab75")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
